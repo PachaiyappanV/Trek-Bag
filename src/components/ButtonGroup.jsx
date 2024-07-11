fde66a5e35072a389e48141d713a6c78867c02a2
PachaiyappanV <pachaiyappan394@gmail.com>
2024-07-11 09:32:17 +0530

    feat: add buttons to button group

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,8 +1,12 @@
+import Button from "./Button";
 
 const ButtonGroup = () => {
-  return (
-    <div>ButtonGroup</div>
-  )
-}
+  return <section className="button-group">
+  <Button text="Mark all as complete" />
+  <Button text="Mark all as incomplete" />
+  <Button text="Reset to inital" />
+  <Button text="Remove all items" />
+  </section>
+};
 
-export default ButtonGroup
+export default ButtonGroup;
