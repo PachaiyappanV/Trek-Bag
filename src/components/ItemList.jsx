a3d7e1a8c9a006e69e0f49167d4881b65ef66ee7
PachaiyappanV <pachaiyappan394@gmail.com>
2024-07-12 18:42:11 +0530

    feat: add sorting logic

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
 import EmptyView from "./EmptyView";
+import Select from "react-select";
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
 
 const ItemList = ({ items, handleDeleteItem, handleToggleItem }) => {
+  const [sortBy, setSortBy] = useState("default");
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    }
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+    return;
+  });
   return (
     <ul className="item-list">
       {items.length == 0 && <EmptyView />}
-      {items.map((item) => {
+      {items.length > 0 && (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            options={sortingOptions}
+            defaultValue={sortingOptions[0]}
+          />
+        </section>
+      )}
+      {sortedItems.map((item) => {
         return (
           <Item
             key={item.id}
